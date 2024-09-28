4d07a3744fbf3686dd1650393dd9a5e571b03e4c
Gain John <johngain494@yahoo.com>
2024-09-28 20:08:48 +0100

    fix order bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Analytics />
         {children}
+        <Analytics />
         <Toaster />
       </body>
     </html>
